b4f8a67061ae24fa69ac4274dde8a1248b9aa7cf
Matt Rangel <mattrangel@gmail.com>
2022-04-18 18:30:43 -0700

    completed app

---
+++ snakes_cafe.py
@@ -0,0 +1,55 @@
+print("**************************************")
+print("**    Welcome to the Snakes Cafe!   **")
+print("**    Please see our menu below.    **")
+print("**")
+print("**To quit at any time, type 'quit'**")
+print("**************************************")
+
+appetizers = ['Wings', 'Cookies', 'Spring Rolls']
+entrees = ['Salmon', 'Steak', 'Meat Tornado', 'A Literal Garden']
+desserts = ['Ice Cream', 'Cake', 'Pie']
+drinks = ['Coffee', 'Tea', 'Unicorn Tears']
+
+print("""
+Appetizers
+----------""")
+# for x in range(len(appetizers)):
+#     # print(appetizers[x])
+for appetizer in appetizers:
+    print(appetizer)
+
+print("""
+Entrees
+-------""")
+for x in range(len(entrees)):
+    print(entrees[x])
+
+print("""
+Desserts
+--------""")
+for x in range(len(desserts)):
+    print(desserts[x])
+
+print("""
+Drinks
+------""")
+for x in range(len(drinks)):
+    print(drinks[x])
+
+print("***********************************")
+print("** What would you like to order? **")
+print("***********************************")
+
+items = {}
+
+while True:
+    order = input('> ')
+    if order == 'quit':
+        break
+    elif order not in items:
+        items[order] = 1
+        
+        print(f"** {items[order]} order of {order} have been added to your meal **")
+    elif order in items:
+        items[order] += 1
+        print(f"** {items[order]} orders of {order} have been added to your meal **")
